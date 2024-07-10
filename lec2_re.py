b6124e1e28952ba2045c9f1cfe97b20a17e7b445
jjl1223 <jjl25301712@gmail.com>
2024-07-10 17:41:53 +0900

    project init

---
+++ lec2_re.py
@@ -0,0 +1,18 @@
+# ctrl + enter :실행
+# shift + 방향키 : 블록
+# shift + Alt + 아래 방향키 : 아래로 복사
+#ctrl + Alt + 아래 방향키: 커서 여러개
+
+a=1
+a
+#. 현재폴더
+#.. 상위폴더
+
+#show folder in new window:
+#해당위치 폴더 열기
+
+
+#파워쉘 명령어 리스트
+#ls: 파일목록
+#cd: 폴더이동 cd 파일이름: 하위폴더 이동 cd .. 상위 폴더 이동
+#cls: 화면정리
